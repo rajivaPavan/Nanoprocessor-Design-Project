c32420eb32f67cea42b6748f6b6f20fadc08327f
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-05-20 23:41:32 +0530

    encapsulated adder

---
+++ components/3-bit-adder/Adder_3.vhd
@@ -0,0 +1,36 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Adder_3 is
+    port (
+        A       : in  std_logic_vector(2 downto 0);
+        B       : in  std_logic_vector(2 downto 0);
+        Cin     : in  std_logic;
+        Sum     : out std_logic_vector(2 downto 0);
+        Cout    : out std_logic
+    );
+end Adder_3;
+
+architecture Behavioral of Adder_3 is
+    component CLA_3 is
+        port (
+            A       : in  std_logic_vector(2 downto 0);
+            B       : in  std_logic_vector(2 downto 0);
+            Cin     : in  std_logic;
+            Sum     : out std_logic_vector(2 downto 0);
+            Cout    : out std_logic
+        );
+    end component;
+    
+begin
+
+    Adder: CLA_3
+    port map (
+            A       => A,
+            B       => B,
+            Cin     => Cin,
+            Sum     => Sum,
+            Cout    => Cout
+        ); 
+    
+end Behavioral;
