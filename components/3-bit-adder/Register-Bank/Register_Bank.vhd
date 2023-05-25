73fa31a48d14caf593f7ca337f7bca585e65143c
sandali <pldsandali@gmail.com>
2023-05-25 21:40:00 +0530

    With the Res

--- components/3-bit-adder/Register-Bank/Register_Bank.vhd
+++ components/3-bit-adder/Register-Bank/Register_Bank.vhd
@@ -4,19 +4,20 @@ use work.COMMON.all;
 
 entity Register_Bank is
     Port ( Reg_En : in STD_LOGIC_VECTOR (2 downto 0);
+           Res : in STD_LOGIC;
            Clk : in STD_LOGIC;
            Data : in STD_LOGIC_VECTOR(3 downto 0);
-           Data_Buses : out buses_8);           
+           Data_Buses : out buses_8);
 end Register_Bank;
 
 architecture Behavioral of Register_Bank is
 
 COMPONENT Reg
     Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
+           Res: in STD_LOGIC;
            En : in STD_LOGIC;
            Clk : in STD_LOGIC;
            Q : out STD_LOGIC_VECTOR (3 downto 0));
-           
 END COMPONENT;
 
 COMPONENT Decoder_3_to_8
@@ -29,12 +30,14 @@ END COMPONENT;
 signal EN_inter : STD_LOGIC;
 signal Reg_sel : STD_LOGIC_VECTOR(7 downto 0);
 
+
 begin
-    --generate an 7 instances of registers
-    registers : for i in 0 to 7 generate            
+
+    registers : for i in 0 to 7 generate
        reg_inst: reg
             port map(
                 D => Data,
+                Res => Res,
                 EN => Reg_Sel(i),
                 Clk => Clk,
                 Q => Data_Buses(i)
@@ -46,8 +49,5 @@ begin
             EN => Clk,
             Y => Reg_Sel
             );
-
-
-
 
 end Behavioral;
