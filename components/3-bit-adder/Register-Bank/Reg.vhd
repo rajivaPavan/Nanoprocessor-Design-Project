96f8150a183edeab7fe93c990dc036b11073dcb6
sandali <pldsandali@gmail.com>
2023-05-25 21:43:06 +0530

    Reg With D_FF

--- components/3-bit-adder/Register-Bank/Reg.vhd
+++ components/3-bit-adder/Register-Bank/Reg.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+
 entity Reg is
     Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
            Res: in STD_LOGIC;
@@ -9,16 +10,46 @@ entity Reg is
            Q : out STD_LOGIC_VECTOR (3 downto 0));
 end Reg;
 
+
 architecture Behavioral of Reg is
+component D_FF
+
+    Port ( D : in STD_LOGIC;
+       Res : in STD_LOGIC;
+       Clk : in STD_LOGIC;
+       Q : out STD_LOGIC;
+       Qbar : out STD_LOGIC);
+end component;
 
 begin
-
-process (Clk) begin
-    if (rising_edge(Clk)) then
-        if EN = '1' then
-            Q <= D;
-        end if;
-    end if;
-end process;
-
+ 
+D_FF_0 : D_FF
+    port map(
+        D => D(0),
+        Res => Res,
+        Clk => Clk,
+        Q => Q(0)
+        );  
+D_FF_1 : D_FF
+    port map(
+        D => D(1),
+        Res => Res,
+        Clk => Clk,
+        Q => Q(1)
+        ); 
+D_FF_2 : D_FF
+    port map(
+        D => D(2),
+        Res => Res,
+        Clk => Clk,
+        Q => Q(2)
+        ); 
+D_FF_3 : D_FF
+    port map(
+        D => D(3),
+        Res => Res,
+        Clk => Clk,
+        Q => Q(3)
+        ); 
+        
 end Behavioral;
