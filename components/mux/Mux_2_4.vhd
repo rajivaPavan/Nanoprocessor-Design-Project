e2d5fc599159cd51c8a44077332f2e6d19887e44
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-05-26 21:54:16 +0530

    updated logic components

--- components/mux/Mux_2_4.vhd
+++ components/mux/Mux_2_4.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use work.logic_components.Mux_2_N;
 
 entity Mux_2_4 is
     port(
@@ -11,16 +11,6 @@ entity Mux_2_4 is
 end Mux_2_4;
 
 architecture Behavioral of Mux_2_4 is
-component Mux_2_N is 
-    generic(
-        N : integer
-    );
-    port(
-        S: in std_logic; -- Control Bit (0 - A, 1 - B)
-        D: in std_logic_vector(2*N - 1 downto 0); -- Data Buses 
-        Y : out std_logic_vector(N-1 downto 0)
-    );
-end component;
 
 begin 
     Mux_2_4_0: Mux_2_N
