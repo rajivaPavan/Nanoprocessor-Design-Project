25f8cd5bc6aaac3d765a3204e475daaa1947d83f
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-05-27 01:49:13 +0530

    working on arithmetic unit

---
+++ components/arithmetic-unit/AU.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.Logic_Components.Add_Sub_4_bit;
+
+entity AU is
+        Port();
+end AU;
+
+architecture Behavioral of Add_Sub_4_bit is
+
+begin
+    
+
+end Behavioral;
+
