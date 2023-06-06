575c68140901b232f8b115045b553437056a1fe9
sandali <pldsandali@gmail.com>
2023-06-06 16:52:30 +0530

    Correct Test Bench

--- tests/TB_Register_Bank.vhd
+++ tests/TB_Register_Bank.vhd
@@ -1,54 +1,64 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.COMMON.all;
 
-
-entity TB_Add_Sub_4_bit is
+entity TB_Register_Bank is
 --  Port ( );
-end TB_Add_Sub_4_bit;
+end TB_Register_Bank;
 
-architecture Behavioral of TB_Add_Sub_4_bit is
+architecture Behavioral of TB_Register_Bank is
 
-COMPONENT Add_Sub_4_bit
-    Port(A_AS : in STD_LOGIC_VECTOR (3 DOWNTO 0);
-         B_AS : in STD_LOGIC_VECTOR (3 DOWNTO 0);
-         CTRL : in STD_LOGIC;
-         S_AS : out STD_LOGIC_VECTOR(3 DOWNTO 0);
-         Zero : out STD_LOGIC;
-         OverFlow : out STD_LOGIC);
+COMPONENT Register_Bank
+    Port ( Reg_En : in STD_LOGIC_VECTOR (2 downto 0);
+           Res : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           Data : in STD_LOGIC_VECTOR(3 downto 0);
+           Data_Buses : out buses_8);
 END COMPONENT;
-signal A, B, S : STD_LOGIC_VECTOR(3 DOWNTO 0) :="0000";
-signal CTRL, Zero : STD_LOGIC:='0';
-signal  OverFlow : STD_LOGIC := '0';
+
+signal Reg_En :  STD_LOGIC_VECTOR (2 downto 0);
+signal Res,Clk : STD_LOGIC;
+signal Data_Buses : buses_8;
+signal Data: STD_LOGIC_VECTOR(3 downto 0);
+constant clk_period : time := 100ns;
 
 begin
 
-uut : Add_Sub_4_bit
-    port map(
-        A_AS => A,
-        B_AS => B,
-        CTRL => CTRL,
-        S_AS => S,
-        Zero => Zero,
-        OverFlow => OverFlow);
-
-process
-begin
-    A <= "0100";  --last digit of 210344          
-    wait for 50 ns; 
-    B <= "0010";  --last digit of 210472
-    wait for 50 ns;
-    CTRL <= '0' ;
-    A <= "1000";  --16        
-    wait for 50 ns; 
-    B <= "0100";  --8
-    wait for 50 ns;
-    --Adding digits of Index Number
-    CTRL <= '0' ;
-    wait for 50 ns;
-    --Subtracting digits of Index Number
-    CTRL <= '1';
-    wait for 50 ns;
-end process;
-        
+uut: Register_Bank
+    port map(Reg_En => Reg_En,
+             Res => Res,
+             Clk => Clk,
+             Data => Data,
+             Data_Buses => Data_Buses);
+    clock: process
+    begin
+        Clk <= '0';
+        wait for clk_period/2;
+        Clk <= '1';
+        wait for clk_period/2;
+    end process;
     
+    stim: process
+    
+    begin
+        Res <= '0';
+        wait for clk_period/2;
+        Reg_En <= "110";    --Enable Reg 6
+        wait for clk_period/2;
+        Data <= "0010";      --Put 2 into the Register 6
+        wait for clk_period/2;
+        Reg_En <= "111";    --Enable Reg 7
+        wait for clk_period/2;
+        Data <= "0101";      --Put 5 into the Register 7
+        wait for clk_period/2;
+        Reg_En <= "110";    --Enable Reg 6
+        wait for clk_period/2;
+        Data <= "0111";     --Put 7 into the Register 6
+        wait for clk_period/2;    
+        Reg_En <= "111";    --Enable Reg 7
+        wait for clk_period/2;   
+        Res <= '1';        --Reset 
+        wait for clk_period/2;
+        
+    end process;       
 end Behavioral;
