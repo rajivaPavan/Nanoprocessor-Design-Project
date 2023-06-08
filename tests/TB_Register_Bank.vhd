bb0becbf17c534dffcc9d9f844ce705fa8c5bbc1
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2023-06-08 11:48:14 +0530

    fixed imports

--- tests/TB_Register_Bank.vhd
+++ tests/TB_Register_Bank.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.COMMON.all;
+use work.buses.Data_Buses;
 
 entity TB_Register_Bank is
 --  Port ( );
@@ -13,12 +13,12 @@ COMPONENT Register_Bank
            Res : in STD_LOGIC;
            Clk : in STD_LOGIC;
            Data : in STD_LOGIC_VECTOR(3 downto 0);
-           Data_Buses : out buses_8);
+           Data_Buses : out Data_Buses);
 END COMPONENT;
 
 signal Reg_En :  STD_LOGIC_VECTOR (2 downto 0);
 signal Res,Clk : STD_LOGIC;
-signal Data_Buses : buses_8;
+signal Data_Buses : Data_Buses;
 signal Data: STD_LOGIC_VECTOR(3 downto 0);
 constant clk_period : time := 100ns;
 
@@ -41,24 +41,20 @@ uut: Register_Bank
     stim: process
     
     begin
+        Res <= '1';
+        wait for clk_period;
         Res <= '0';
-        wait for clk_period/2;
         Reg_En <= "110";    --Enable Reg 6
-        wait for clk_period/2;
         Data <= "0010";      --Put 2 into the Register 6
-        wait for clk_period/2;
+        wait for clk_period;
         Reg_En <= "111";    --Enable Reg 7
-        wait for clk_period/2;
         Data <= "0101";      --Put 5 into the Register 7
-        wait for clk_period/2;
+        wait for clk_period;
         Reg_En <= "110";    --Enable Reg 6
-        wait for clk_period/2;
         Data <= "0111";     --Put 7 into the Register 6
-        wait for clk_period/2;    
+        wait for clk_period;    
         Reg_En <= "111";    --Enable Reg 7
-        wait for clk_period/2;   
         Res <= '1';        --Reset 
-        wait for clk_period/2;
-        
+        wait for clk_period;
     end process;       
 end Behavioral;
